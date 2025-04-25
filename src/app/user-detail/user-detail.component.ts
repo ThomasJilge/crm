c7c313539929ec0c822f02b6b0f4d77f0a326ded
ThomasJilge <thomasjilge@gmx.de>
2025-04-25 19:58:47 +0200

    user id added

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-detail',
@@ -11,5 +12,18 @@ import { MatCardModule } from '@angular/material/card';
   styleUrl: './user-detail.component.scss'
 })
 export class UserDetailComponent {
+
+  userId = '';
+
+constructor (private route: ActivatedRoute) { 
+}
+
+ngOnInit(): void {
+  this.route.paramMap.subscribe( paramMap => {
+    this.userId = paramMap.get('id') || '';
+    console.log('GOT ID', this.userId)
+  })
+  
+}
 
 }
